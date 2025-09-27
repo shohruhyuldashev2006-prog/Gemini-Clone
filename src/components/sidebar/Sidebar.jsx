5645d5b3834aa8464964eb45514f9e8d2a38f928
Shohrux Yuldashev <sizning_emailingiz@example.com>
2025-09-27 16:36:23 +0500

    update: loyiha ozgartirildi

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,7 +4,7 @@ import { useContext, useState } from 'react';
 import { Context } from '../../context/Context';
 function Sidebar() {
   const [extended, setExtended] = useState(false);
-  const { onSent, prevPrompt, setRecentPrompt } = useContext(Context);
+  const { onSent, prevPrompt, setRecentPrompt, newChat } = useContext(Context);
 
   const loaderPrompt = async (prompt) => {
     setRecentPrompt(prompt);
@@ -19,7 +19,7 @@ function Sidebar() {
           src={assets.menu_icon}
           alt=""
         />
-        <div className="new-chat">
+        <div onClick={() => newChat()} className="new-chat">
           <img src={assets.plus_icon} alt="" />
           {extended ? <p>New Chat</p> : null}
         </div>
